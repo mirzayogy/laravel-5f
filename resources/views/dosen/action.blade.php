ac7c5bc88638e48e55629a2b3bb7de44e203fef1
mirzayogy <mirza.yogy@gmail.com>
2025-10-21 20:22:12 +0800

    new: tampilan data dosen

---
+++ resources/views/dosen/action.blade.php
@@ -0,0 +1,6 @@
+<a href="#" class="btn btn-sm btn-primary">
+    Edit
+</a>
+<a href="#">
+    Hapus
+</a>
--- resources/views/dosen/index.blade.php
+++ resources/views/dosen/index.blade.php
@@ -1,1 +1,37 @@
-{{ dd($banyak_dosen) }}
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+<table>
+    <tr>
+        <th>Id</th>
+        <th>Nama Dosen</th>
+        <th>Nomor Induk</th>
+        <th>Gaji</th>
+        <th>Tanggal Lahir</th>
+        <th>Opsi</th>
+    </tr>
+    @foreach ($banyak_dosen as $dosen)
+    <tr>
+        <td> {{ $dosen['id'] }} </td>
+        <td> {{ $dosen['nama_dosen'] }} </td>
+        <td> {{ $dosen['nomor_induk_dosen'] }} </td>
+        <td> {{ $dosen['gaji'] }} </td>
+        <td> {{ $dosen['tanggal_lahir'] }} </td>
+        <td> @include('dosen.action') </td>
+    </tr>
+    @endforeach
+</table>
