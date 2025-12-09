841241c0456c00892b56b46a4fa7248ad4fcc58b
mirzayogy <mirza.yogy@gmail.com>
2025-12-09 20:14:47 +0800

    perbaikan halaman index

--- resources/views/dosen/index.blade.php
+++ resources/views/dosen/index.blade.php
@@ -1,54 +1,53 @@
 @extends('template.default')
-<style>
-    table {
-        font-family: arial, sans-serif;
-        border-collapse: collapse;
-        width: 100%;
-    }
-
-    td,
-    th {
-        border: 1px solid #dddddd;
-        text-align: left;
-        padding: 8px;
-    }
-
-    tr:nth-child(even) {
-        background-color: #dddddd;
-    }
-</style>
 @section('content')
-    <h1 class="h3 mb-4 text-gray-800">Dosen</h1>
     @if (session('berhasil'))
         {{ session('berhasil') }}
         <br>
     @endif
-    <a href="{{ route('dosen.create') }}">Tambah Data</a>
-    <table>
-        <tr>
-            <th>Id</th>
-            <th>Nama Dosen</th>
-            <th>Nomor Induk</th>
-            <th>Gaji</th>
-            <th>Tanggal Lahir</th>
-            <th>Opsi</th>
-        </tr>
-        @foreach ($banyak_dosen as $dosen)
-            <tr>
-                <td> {{ $dosen['id'] }} </td>
-                <td> {{ $dosen['nama_dosen'] }} </td>
-                <td> {{ $dosen['nomor_induk_dosen'] }} </td>
-                <td> {{ $dosen['gaji'] }} </td>
-                <td> {{ $dosen['tanggal_lahir'] }} </td>
-                <td> @include('dosen.action') </td>
-            </tr>
-        @endforeach
-    </table>
-
-    <form action="" method="post" id="deleteForm">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Dosen</h1>
+        <a href="{{ route('dosen.create') }}" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm">
+            <i class="fas fa-plus fa-sm text-white-50"></i> Tambah
+        </a>
+    </div>
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Data Dosen</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <th>ID</th>
+                        <th>Nama</th>
+                        <th>NIDN</th>
+                        <th>Gaji</th>
+                        <th>Tanggal Lahir</th>
+                        <th>Opsi</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($banyak_dosen as $dosen)
+                            <tr>
+                                <td>{{ $dosen['id'] }}</td>
+                                <td>{{ $dosen['nama_dosen'] }}</td>
+                                <td>{{ $dosen['nomor_induk_dosen'] }}</td>
+                                <td>{{ $dosen['gaji'] }}</td>
+                                <td>{{ $dosen['tanggal_lahir'] }}</td>
+                                <td style="align-items: center">
+                                    <a href="{{ route('dosen.edit', $dosen) }}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> Edit</a>
+                                    <button class="btn btn-danger btn-sm"
+                                        onclick="konfirmasi('{{ route('dosen.destroy', $dosen) }}')"><i class="fa fa-trash"></i> Hapus</button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <form action="" method="POST" id="deleteForm">
         @csrf
         @method('DELETE')
-        <input type="submit" value="Hapus" style="display: none">
     </form>
     <script>
         function konfirmasi(href) {
@@ -59,3 +58,13 @@
         }
     </script>
 @endsection
+
+@push('styles')
+    <link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+@endpush
+
+@push('scripts')
+    <script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('js/demo/datatables-demo.js') }}"></script>
+@endpush
--- resources/views/template/components/scripts.blade.php
+++ resources/views/template/components/scripts.blade.php
@@ -7,3 +7,5 @@
 
 <!-- Custom scripts for all pages-->
 <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+
+@stack('scripts')
